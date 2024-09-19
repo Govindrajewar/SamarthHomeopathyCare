52bddf5170a5d99759ee1208fdcca284b4767862
Govind Rajewar <abhirajewar87@gmail.com>
2024-09-19 10:11:38 +0530

    getUserData added to fetch current user

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { message } from "antd";
+import axios from "axios";
 
 function HomePage() {
+  const getUserData = async () => {
+    try {
+      const response = await axios.post(
+        "/user/getUserData",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log("User data:", response.data);
+    } catch (error) {
+      message.error("Failed to fetch user data.");
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div>
       HomePage Page
@@ -8,4 +31,4 @@ function HomePage() {
   )
 }
 
-export default HomePage
+export default HomePage;
